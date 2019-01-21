457fa99f933d6ea7263696806d92e1a56bf776c7
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-21 15:47:04 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'sessions#create'
   resources :sessions
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
   get '/secrets' => 'secrets#show'
 end
