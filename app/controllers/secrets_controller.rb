41ca6c53789fcd445aa41575d7099c1087c12ab0
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-21 15:39:15 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,5 +1,10 @@
 class SecretsController < ApplicationController
   before_action :require_login
   
-  
-end 
+  private
+  def require_login
+    unless session.include? :name
+      redirect_to :login
+    end
+  end
+end
